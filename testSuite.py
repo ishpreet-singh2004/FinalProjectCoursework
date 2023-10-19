45caf350e3858cf0285e0b075f138cd2a6ffb1c8
ishpreet-singh2004 <ec22980@qmul.ac.uk>
2023-10-19 10:32:32 +0100

    test

---
+++ testSuite.py
@@ -0,0 +1,19 @@
+import unittest
+import unitTesting
+
+class testingFunction(unittest.TestCase):
+
+    def firstTest(self):
+        result = subtractionText.subtract_numbers(10, 7)
+        self.assertEqual(result,3)
+    
+    def secondTest(self):
+        result = subtractionText.subtract_numbers(2, 7)
+        self.assertEqual(result,-5)
+    
+    def thirdTest(self):
+        result = subtractionText.subtract_numbers(5.5, 2.0)
+        self.assertEqual(result,3.0)
+        
+if __name__ == '__main__':
+    unittest.main()
