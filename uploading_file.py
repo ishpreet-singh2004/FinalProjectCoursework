4a395ae273e75e2ecfdd2651061d966263c104f9
Ishpreet Singh <ec22980@qmul.ac.uk>
2024-01-11 10:05:11 +0000

    here i have uploaded the csv fine into folder

---
+++ uploading_file.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt 
+import numpy as np
+import pandas as pd
+
+#Code to display and call data which I am analysing (csv file).
+
+pd.options.display.max_rows = 700
+
+data = pd.read_csv('vscode final project data CSV.csv')
+print(data)
+
+
