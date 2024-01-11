fb42336c000c4f29d3f076cbbe487c183276a57d
Ishpreet Singh <ec22980@qmul.ac.uk>
2024-01-11 10:06:54 +0000

    here i can view and import the data

--- uploading_file.py
+++ uploading_file.py
@@ -1,12 +1,14 @@
+from sys import displayhook
+from tkinter.tix import DisplayStyle
 import matplotlib.pyplot as plt 
 import numpy as np
 import pandas as pd
 
 #Code to display and call data which I am analysing (csv file).
 
-pd.options.display.max_rows = 700
+pd.options.display.max_rows = 400
 
 data = pd.read_csv('vscode final project data CSV.csv')
-print(data)
+displayhook(data)
 
 
