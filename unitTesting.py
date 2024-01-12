1a98dcd14ba76adb5a6c4d7507cc7986ffc37285
Ishpreet Singh <ec22980@qmul.ac.uk>
2024-01-12 09:13:55 +0000

    linear graphs for both variables

--- unitTesting.py
+++ unitTesting.py
@@ -8,3 +8,53 @@ def creating_the_graphs(data, graph_type='stacked_area', countries=None):
     # - graph_type: str, the type of graph to create. Options: 'linear graph (food/undernourishment)', 'heatmap (food/undernourishment)' , 'grouped_bar (food/undernourishment)', 'bubble_chart'.
     # - countries: list, a list of country names to include in the graph (applies to 'grouped_bar' graph_type).
     # - returns the different types of graphs which i have made using the data. 
+
+    if graph_type == 'line_graph_food':
+        # Stacked Area Chart
+        # List of unique countries in the dataset
+        countries = data['Entity'].unique()
+
+        # Create a separate plot for each country
+        for country in countries:
+        # Filter data for the specific country
+            country_data = data[data['Entity'] == country]
+
+        # Plotting
+            plt.figure(figsize=(10, 6))
+            plt.plot(country_data['Year'], country_data['Total food expenditure per year'], label='Food Expenditure')
+
+            # Adding labels and title
+            plt.xlabel('Year')
+            plt.ylabel('Value')
+            plt.title(f'{country} - Food Expenditure Over the Years')
+
+            # Adding legend
+            plt.legend()
+
+            # Show the plot
+            plt.show()
+
+    elif graph_type == 'line_graph_undernourishment':
+        # Grouped Bar Chart
+        # List of unique countries in the dataset
+        countries = data['Entity'].unique()
+
+        # Create a separate plot for each country
+        for country in countries:
+            # Filter data for the specific country
+            country_data = data[data['Entity'] == country]
+
+            # Plotting
+            plt.figure(figsize=(10, 6))
+            plt.plot(country_data['Year'], country_data['Prevalence of undernourishment (%)'], label='Undernourishment')
+
+            # Adding labels and title
+            plt.xlabel('Year')
+            plt.ylabel('Value')
+            plt.title(f'{country} - Undernourishment Over the Years')
+
+            # Adding legend
+            plt.legend()
+
+            # Show the plot
+            plt.show()
