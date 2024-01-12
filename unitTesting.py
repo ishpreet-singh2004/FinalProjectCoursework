99e0d1a56613062f1011d5ec2eabf8a6fe283c8a
Ishpreet Singh <ec22980@qmul.ac.uk>
2024-01-12 09:08:35 +0000

    Start of the function for the unit testing

--- unitTesting.py
+++ unitTesting.py
@@ -1,2 +1,5 @@
-def subtract_numbers(a, b):
-    return a - b
+def creating_the_graphs(data, graph_type='stacked_area', countries=None):
+    
+    #Create complex and informative graphs based on the data which i have cleansed. 
+    #Here all of the possible outcomes for all the graphs which i have created. 
+    
